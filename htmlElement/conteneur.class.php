32ad499628ff7d0b086b8b0456d75dc8a6d87d64
leseulsteve <leseulsteve@gmail.com>
2014-04-23 11:34:26 -0400

    modifs

--- htmlElement/conteneur.class.php
+++ htmlElement/conteneur.class.php
@@ -1,11 +1,40 @@
 <?php
 
-class HtmlElement_Conteneur extends HtmlElement_ClassesAbstraites_ConteneurAbstraite
+class HtmlElement_Conteneur extends HtmlElement_ClassesAbstraites_HtmlElement
 {
+	private $elements = array();
+	private $horizontal = false;
+
 	/**
  	 * Constructeur d'un conteneur html.
  	 */
 	public function __construct(){}
 
+	/**
+ 	 * Ajout d'un élément au conteneur.
+ 	 *
+ 	 * @param Objet $element L'élément à ajouter.
+ 	 */
+	public function ajout($element)
+	{
+		array_push($this->elements, $element);
+
+		if ($this->horizontal)
+		{
+			$config = Configuration::get_instance();
+			$theme = $config->getTheme();
+			$theme->setHorizontal($this);
+		}
+	}
+	
+	/**
+ 	 * Accesseur des éléments du conteneur.
+ 	 *
+ 	 * @return Array Les éléments du conteneur.
+ 	 */
+	public function getElements()
+	{
+		return $this->elements;
+	}
 }
 ?>
