a6e7cccb1b53a9f4939154a062e1df02a6a04d17
leseulsteve <leseulsteve@gmail.com>
2014-04-23 07:46:08 -0400

    Update objetAvecTemplate.class.php

--- classesAbstraites/objetAvecTemplate.class.php
+++ classesAbstraites/objetAvecTemplate.class.php
@@ -6,7 +6,13 @@ abstract class classesAbstraites_ObjetAvecTemplate
  	 */
 	public function render()
 	{
-		$reflector = new ReflectionClass(get_class($this));
-		include dirname($reflector->getFileName()) . '/templates/' . basename($reflector->getFileName(), '.class.php') . '.tpl.php'; 
+		$reflecteur = new ReflectionClass(get_class($this));
+		$nomFichier = basename($reflecteur->getFileName(), '.class.php') . '.tpl.php';
+		$cheminDefaut = $_SERVER['DOCUMENT_ROOT'] . '/test/themes/defaut/' . $nomFichier;
+
+		$cheminTheme = $_SERVER['DOCUMENT_ROOT'] . '/test/themes/bootstrap3/';
+		$cheminThemeFichier = $cheminTheme . $nomFichier;
+
+		include file_exists($cheminThemeFichier) ? $cheminThemeFichier : $cheminDefaut; 
 	}
 }
