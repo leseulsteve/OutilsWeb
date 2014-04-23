30cbd09fac0c83650aa78f55a758cf168d4c112d
leseulsteve <leseulsteve@gmail.com>
2014-04-23 07:46:41 -0400

    Create themeAbstraite.class.php

---
+++ classesAbstraites/themeAbstraite.class.php
@@ -0,0 +1,10 @@
+<?php
+abstract class ClassesAbstraites_ThemeAbstraite implements ClassesInterfaces_ThemeInterface
+{
+	protected function getNomClasse($objet)
+	{
+		$reflecteur = new ReflectionClass(get_class($objet));
+		return basename($reflecteur->getFileName(), '.class.php');
+	}
+}
+?>
